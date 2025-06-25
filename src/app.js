3c7db56cf2de39a88169f279f9c90a6634bf9bae
mohammad <mohamedwork216@gmail.com>
2025-06-25 17:48:47 +0300

    setup the structure of the project

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+// Middlewares
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Health check
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+module.exports = app;

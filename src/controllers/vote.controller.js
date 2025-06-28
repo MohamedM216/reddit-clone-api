62a120dfc353026d4b69231c8a6a8eb321da7a41
mohammad <mohamedwork216@gmail.com>
2025-06-28 19:38:44 +0300

    add vote controller

---
+++ src/controllers/vote.controller.js
@@ -0,0 +1,62 @@
+const voteService = require('../services/vote.service');
+
+class VoteController {
+  async upvote(req, res, next) {
+    try {
+      const { postId, commentId } = req.params;
+      const result = await voteService.vote(
+        req.user.id,
+        { postId, commentId },
+        1,
+        req.io
+      );
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async downvote(req, res, next) {
+    try {
+      const { postId, commentId } = req.params;
+      const result = await voteService.vote(
+        req.user.id,
+        { postId, commentId },
+        -1, 
+        req.io
+      );
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async removeVote(req, res, next) {
+    try {
+      const { postId, commentId } = req.params;
+      const result = await voteService.removeVote(
+        req.user.id,
+        { postId, commentId },
+        req.io
+      );
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getVote(req, res, next) {
+    try {
+      const { postId, commentId } = req.params;
+      const vote = await voteService.getVote(
+        req.user.id,
+        { postId, commentId }
+      );
+      res.json({ vote });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = new VoteController();

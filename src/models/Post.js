ce85e6603e054aa00a156affd0210d7a0ec09c35
mohammad <mohamedwork216@gmail.com>
2025-07-10 15:55:02 +0300

    update db schema and post model

--- src/models/Post.js
+++ src/models/Post.js
@@ -3,14 +3,12 @@ class Post {
     id,
     user_id,
     content,
-    image_url,
     vote_count,
     created_at
   }) {
     this.id = id;
     this.userId = user_id;
     this.content = content;
-    this.imageUrl = image_url;
     this.voteCount = vote_count || 0;
     this.createdAt = created_at;
   }
@@ -20,9 +18,8 @@ class Post {
       id: this.id,
       userId: this.userId,
       content: this.content,
-      imageUrl: this.imageUrl,
       voteCount: this.voteCount,
-      createdAt: this.createdAt,
+      createdAt: this.createdAt
     };
   }
 }
